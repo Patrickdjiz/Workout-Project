d926f6f90fbd0f85953fb2a7c572296d3f45c714
Patrickdjiz <djizpatrick@gmail.com>
2024-02-04 16:17:19 +1100

    x

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Installed packages:
 npm install express
 npm install -g nodemon
 npm install dotenv (loads environment variables from a .env file into the process.env object)
+npm install mongoose (mongoose wraps mongoDB to be able to use methods to read and write database documents)
 
 Commands:
 nodemon server.js (starts the server and updates changes live)
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config() // require the dotenv package so we can use the process object to access our .env file with our hidden variables
 
 const express = require('express') // requiring the express package to be used in the code
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts') // imports the routes from our workouts.js file 
 
 const app = express() // express app being stored in app constant
@@ -19,8 +20,17 @@ app.use((req, res, next) => {
 // uses the routes we imported on the app
 app.use('/api/workouts', workoutRoutes) // when a request is fired to the /api/workouts path then we invoke any route. This path is just a normal '/' inside the workouts.js file
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+// connect to mongoDB and mongoose
+// we do not want to listen to requests unless we are connected to our database so we do that inside here
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+        console.log('connected to db and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 
