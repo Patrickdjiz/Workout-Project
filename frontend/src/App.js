4e1ce50c90db32a21a675572b51a080f6aaf0c72
Patrickdjiz <djizpatrick@gmail.com>
2024-02-05 22:56:31 +1100

    x

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@
 // Route to create a single route
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+import React from 'react'; // importing react
+
 // import pages & components
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route 
